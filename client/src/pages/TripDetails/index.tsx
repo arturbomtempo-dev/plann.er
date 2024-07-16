479e81c04872ab37786c74ae0ce5402b7f58deb5
Artur Bomtempo Colen <arturbcolen@gmail.com>
2024-07-16 18:42:41 -0300

    feat(client): addition of the modal to register activities

--- client/src/pages/TripDetails/index.tsx
+++ client/src/pages/TripDetails/index.tsx
@@ -6,10 +6,24 @@ import {
     MapPin,
     Plus,
     Settings2,
+    Tag,
+    User,
     UserCog,
+    X,
 } from 'lucide-react';
+import { useState } from 'react';
 
 export function TripDetailsPage() {
+    const [isCreateActivityModalOpen, setIsCreateActivityModalOpen] = useState(false);
+
+    function openCreateActivityModal() {
+        setIsCreateActivityModalOpen(true);
+    }
+
+    function closeCreateActivityModal() {
+        setIsCreateActivityModalOpen(false);
+    }
+
     return (
         <div className="max-w-6xl px-6 py-10 mx-auto space-y-8">
             <div className="px-4 h-16 rounded-xl bg-zinc-900 shadow-shape flex items-center justify-between">
@@ -33,7 +47,10 @@ export function TripDetailsPage() {
                 <div className="flex-1 space-y-6">
                     <div className="flex items-center justify-between">
                         <h2 className="text-3xl font-semibold">Atividades</h2>
-                        <button className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
+                        <button
+                            onClick={openCreateActivityModal}
+                            className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400"
+                        >
                             <Plus className="size-5" />
                             Cadastrar atividade
                         </button>
@@ -41,12 +58,8 @@ export function TripDetailsPage() {
                     <div className="space-y-8">
                         <div className="space-y-2.5">
                             <div className="flex gap-2 items-baseline">
-                                <span className="text-xl text-zinc-300 font-semibold">
-                                    Dia 17
-                                </span>
-                                <span className="text-xs text-zinc-500">
-                                    Sábado
-                                </span>
+                                <span className="text-xl text-zinc-300 font-semibold">Dia 17</span>
+                                <span className="text-xs text-zinc-500">Sábado</span>
                             </div>
                             <p className="text-zinc-500 text-sm">
                                 Nenhuma atividade cadastrada nessa data.
@@ -54,33 +67,21 @@ export function TripDetailsPage() {
                         </div>
                         <div className="space-y-2.5">
                             <div className="flex gap-2 items-baseline">
-                                <span className="text-xl text-zinc-300 font-semibold">
-                                    Dia 18
-                                </span>
-                                <span className="text-xs text-zinc-500">
-                                    Domingo
-                                </span>
+                                <span className="text-xl text-zinc-300 font-semibold">Dia 18</span>
+                                <span className="text-xs text-zinc-500">Domingo</span>
                             </div>
                             <div className="space-y-2.5">
                                 <div className="px-4 py-2.5 bg-zinc-900 rounded-xl shadow-shape flex items-center gap-3">
                                     <CircleCheck className="size-5 text-lime-300" />
-                                    <span className="text-zinc-100">
-                                        Academia em grupo
-                                    </span>
-                                    <span className="text-zinc-400 text-sm ml-auto">
-                                        08:00h
-                                    </span>
+                                    <span className="text-zinc-100">Academia em grupo</span>
+                                    <span className="text-zinc-400 text-sm ml-auto">08:00h</span>
                                 </div>
                             </div>
                             <div className="space-y-2.5">
                                 <div className="px-4 py-2.5 bg-zinc-900 rounded-xl shadow-shape flex items-center gap-3">
                                     <CircleCheck className="size-5 text-lime-300" />
-                                    <span className="text-zinc-100">
-                                        Almoço
-                                    </span>
-                                    <span className="text-zinc-400 text-sm ml-auto">
-                                        12:00h
-                                    </span>
+                                    <span className="text-zinc-100">Almoço</span>
+                                    <span className="text-zinc-400 text-sm ml-auto">12:00h</span>
                                 </div>
                             </div>
                         </div>
@@ -88,9 +89,7 @@ export function TripDetailsPage() {
                 </div>
                 <div className="w-80 space-y-6">
                     <div className="space-y-6">
-                        <h2 className="font-semibold text-xl">
-                            Links importantes
-                        </h2>
+                        <h2 className="font-semibold text-xl">Links importantes</h2>
                         <div className="space-y-5">
                             <div className="flex items-center justify-between gap-4">
                                 <div className="space-y-1.5">
@@ -162,6 +161,49 @@ export function TripDetailsPage() {
                     </div>
                 </div>
             </main>
+            {isCreateActivityModalOpen && (
+                <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
+                    <div className="w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
+                        <div className="space-y-2">
+                            <div className="flex items-center justify-between">
+                                <h2 className="text-lg font-semibold">Cadastrar atividade</h2>
+                                <button type="button" onClick={closeCreateActivityModal}>
+                                    <X className="size-5 text-zinc-400" />
+                                </button>
+                            </div>
+                            <p className="text-sm text-zinc-400">
+                                Todos convidados podem visualizar as atividades.
+                            </p>
+                        </div>
+                        <form className="space-y-3">
+                            <div className="h-14 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
+                                <Tag className="text-zinc-400 size-5" />
+                                <input
+                                    type="text"
+                                    name="title"
+                                    placeholder="Qual a atividade?"
+                                    className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
+                                />
+                            </div>
+                            <div className="h-14 flex-1 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
+                                <User className="text-zinc-400 size-5" />
+                                <input
+                                    type="datetime-local"
+                                    name="occurs_at"
+                                    placeholder="Data e horário da atividade"
+                                    className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
+                                />
+                            </div>
+                            <button
+                                type="submit"
+                                className="bg-lime-300 w-full justify-center text-lime-950 rounded-lg px-5 h-11 font-medium flex items-center gap-2 hover:bg-lime-400"
+                            >
+                                Salvar atividade
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            )}
         </div>
     );
 }
