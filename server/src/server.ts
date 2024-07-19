b11c3a35ae9b069d38f38886972c455b91228b57
Artur Bomtempo Colen <arturbcolen@gmail.com>
2024-07-19 16:04:24 -0300

    feat(server): configuring the prisma ORM

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,27 @@
-console.log('Hello World!');
+import fastfy from 'fastify';
+import { prisma } from './lib/prisma';
+
+const PORT = Number(process.env.PORT) || 3000;
+const app = fastfy();
+
+app.get('/cadastrar', async () => {
+    await prisma.trip.create({
+        data: {
+            destination: 'Belo Horizonte',
+            starts_at: new Date(),
+            ends_at: new Date(),
+        },
+    });
+
+    return 'Registro cadastrado com sucesso!';
+});
+
+app.get('/listar', async () => {
+    const trips = await prisma.trip.findMany();
+
+    return trips;
+});
+
+app.listen({ port: PORT }).then(() => {
+    console.log(`Server is running on port ${PORT}`);
+});
