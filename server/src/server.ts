1fcef445b40b537547c1d43cb6c51492d009666c
Artur Bomtempo Colen <arturbcolen@gmail.com>
2024-07-23 14:00:55 -0300

    feat(server): adding routes linked to activities and links

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,6 +3,11 @@ import cors from '@fastify/cors';
 import { serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod';
 import { createTrip } from './routes/create-trip';
 import { confirmTrip } from './routes/confirm-trip';
+import { confirmParticipant } from './routes/confirm-participant';
+import { createActivity } from './routes/create-activity';
+import { getActivities } from './routes/get-activities';
+import { createLink } from './routes/create-link';
+import { getLinks } from './routes/get-links';
 
 const PORT = Number(process.env.PORT) || 3000;
 const app = fastfy();
@@ -16,6 +21,11 @@ app.setSerializerCompiler(serializerCompiler);
 
 app.register(createTrip);
 app.register(confirmTrip);
+app.register(confirmParticipant);
+app.register(createActivity);
+app.register(getActivities);
+app.register(createLink);
+app.register(getLinks);
 
 app.listen({ port: PORT }).then(() => {
     console.log(`Server is running on port ${PORT}`);
